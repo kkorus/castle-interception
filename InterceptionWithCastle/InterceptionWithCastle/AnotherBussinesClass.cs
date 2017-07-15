f95ed3bcba32f8ef3b75d57423256179c056a28a
Konrad Korus <konradkorus@gmail.com>
2017-07-15 12:24:50 +0100

    Initial commit

---
+++ InterceptionWithCastle/InterceptionWithCastle/AnotherBussinesClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterceptionWithCastle
+{
+    public class AnotherBussinesClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("I am AnotherBussinesClass");
+        }
+    }
+}
---
+++ InterceptionWithCastle/InterceptionWithCastle/Program.cs
@@ -0,0 +1,25 @@
+﻿using Castle.DynamicProxy;
+
+namespace InterceptionWithCastle
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var proxyGenerator = new ProxyGenerator();
+
+            var obj = proxyGenerator
+                .CreateClassProxy<MotherFBussinesClass>(
+                    new MyInterceptorAspect()
+                );
+
+            var obj2 = proxyGenerator
+                .CreateClassProxy<AnotherBussinesClass>(
+                    new MyInterceptorAspect()
+                );
+
+            obj.Method1();
+            obj2.Method1();
+        }
+    }
+}
